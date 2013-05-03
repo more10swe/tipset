44440d6fd747e5ee7d64caa96b621e811843603a
Mårten Sundh <martensundh@gmail.com>
2013-05-03 16:53:10 +0200

    Nu kan man inte längre tippa matcher som har startat. Klockslagen visas ordentligt och även oddsen sparas rätt. I tooltipen visas 1-X-2-oddsen (men inte snyggt).

--- common/posttomatcher.php
+++ common/posttomatcher.php
@@ -17,46 +17,57 @@ $filename = "datan.xml";
 
 $oddsfeed = simplexml_load_file($filename);
 
-$hemmalag = $oddsfeed->fd->sports->sport->leagues->league->events->event->homeTeam->name;
-$bortalag = $oddsfeed->fd->sports->sport->leagues->league->events->event->awayTeam->name;
-$matchtid = $oddsfeed->fd->sports->sport->leagues->league->events->event->startDateTime;
-$matchid = $oddsfeed->fd->sports->sport->leagues->league->events->event->id;
-
-# För att se strukturen enkelt.
-#<rsp status="ok"><fd><sports><sport><leagues><league><events><event><startDateTime>2013-04-12T16:59:00Z</startDateTime><id>296512768</id><homeTeam><name>Brommapojkarna</name><rotNum>5901</rotNum></homeTeam><awayTeam><name>IFK Norrkoping FK
-#
-echo $hemmalag . " - " . $bortalag;
-
-// två sql-satser
-$queryinsert = "INSERT INTO MATCHER VALUES ('$matchid','$hemmalag','$bortalag','$matchtid')";
-$queryupdate = "UPDATE MATCHER SET HEMMALAG='$hemmalag', BORTALAG='$bortalag', MATCHTID='$matchtid' WHERE 'MATCH-ID'='$matchid'";
-
-echo "<br />";
-
-
-// En snäll notering: MAN MÅSTE ANVÄNDA SÅ KALLADE BACKTICKS NÄR SQL-TABELLEN ELLER KOLUMNEN INNEHÅLLER ETT BINDESTRECK ELLER LIKNANDE. ALLTSÅ SÅNAHÄRA: `
-$simplequery = mysqli_query($connection, "SELECT `MATCH-ID` FROM MATCHER HAVING `MATCH-ID`='$matchid'");
-$results = mysqli_fetch_assoc($simplequery);
-echo '<pre>'.print_r($results,true).'</pre>';
-echo "Kan det bli en tvåa? " . $results['MATCH-ID'] . "<br />";
-
-
-if($results['MATCH-ID']!='')
+foreach ($oddsfeed->fd->sports->sport->leagues->league->events->event as $match) //Stor slinga som stoppar in alla matcherna i databasen!
 {
-	// utför själva frågan
-	mysqli_query($connection, $queryupdate);
-	    //or die(header("location:pinnacleapitest.php"));
-		or die();
+
+	$hemmalag = $match->homeTeam->name;
+	$bortalag = $match->awayTeam->name;
+	$matchtid = $match->startDateTime;
+	$matchid = $match->id;
+
+	# För att se strukturen enkelt.
+	#<rsp status="ok"><fd><sports><sport><leagues><league><events><event><startDateTime>2013-04-12T16:59:00Z</startDateTime><id>296512768</id><homeTeam><name>Brommapojkarna</name><rotNum>5901</rotNum></homeTeam><awayTeam><name>IFK Norrkoping FK
+	#
+	echo $hemmalag . " - " . $bortalag;
+
+	// två sql-satser
+	$queryinsert = "INSERT INTO MATCHER VALUES ('$matchid','$hemmalag','$bortalag','$matchtid')";
+	$queryupdate = "UPDATE MATCHER SET HEMMALAG='$hemmalag', BORTALAG='$bortalag', MATCHTID='$matchtid' WHERE 'MATCH-ID'='$matchid'";
+
+	echo "<br />";
+
+
+	// En snäll notering: MAN MÅSTE ANVÄNDA SÅ KALLADE BACKTICKS NÄR SQL-TABELLEN ELLER KOLUMNEN INNEHÅLLER ETT BINDESTRECK ELLER LIKNANDE. ALLTSÅ SÅNAHÄRA: `
+	$simplequery = mysqli_query($connection, "SELECT `MATCH-ID` FROM MATCHER HAVING `MATCH-ID`='$matchid'");
+	$results = mysqli_fetch_assoc($simplequery);
+	echo '<pre>'.print_r($results,true).'</pre>';
+	echo "Kan det bli en tvåa? " . $results['MATCH-ID'] . "<br />";
+
+
+	if($results['MATCH-ID']!="")
+	{
+		// utför själva frågan
+		mysqli_query($connection, $queryupdate);
+		    //or die(header("location:pinnacleapitest.php"));
+			//or die();
+	}
+	else
+	{
+		// utför själva frågan
+		mysqli_query($connection, $queryinsert);
+		    //or die(header("location:pinnacleapitest.php"));
+			//or die();
+	}
 }
-else
-{
-	// utför själva frågan
-	mysqli_query($connection, $queryinsert);
-	    //or die(header("location:pinnacleapitest.php"));
-		or die();
-}
+
+/* close connection */
+mysqli_close($connection);
+
 ?>
+
+
 <html>
+
 	<head>
 		<title>
 			Postar!
--- common/posttotips.php
+++ common/posttotips.php
@@ -19,11 +19,44 @@ $matchid = $_POST['matchid'];
 $hemmamal_t = $_POST['hemmamal'];
 $bortamal_t = $_POST['bortamal'];
 
-print $tipparid . " " . $matchid . " " . $hemmamal_t . " " . $bortamal_t;
+// Nu ska vi bara hämta oddset för det rätta tecknet!
+
+$filename = "datan.xml";
+$oddsfeed = simplexml_load_file($filename);
+
+foreach ($oddsfeed->fd->sports->sport->leagues->league->events->event as $match) //Stor slinga som stoppar in alla matcherna i databasen!
+{
+	if ($match->id == $matchid) 
+	{
+		foreach ($match->periods->period as $oddsfeed2) 
+		{
+			if ($hemmamal_t>$bortamal_t) 
+			{
+				$odds = $oddsfeed2->moneyLine->homePrice;
+			}
+			else if ($hemmamal_t==$bortamal_t) 
+			{
+				$odds = $oddsfeed2->moneyLine->drawPrice;
+			}
+			else if ($hemmamal_t<$bortamal_t) 
+			{
+				$odds = $oddsfeed2->moneyLine->awayPrice;
+			}
+			else
+			{
+				print_r("Något knas med resultatjämförelse.");
+			}
+		}
+	}
+}
+
+
+
+print $tipparid . " " . $matchid . " " . $hemmamal_t . " " . $bortamal_t . " " . $odds;
 
 // två sql-satser
-$queryinsert = "INSERT INTO TIPS VALUES ('$tipparid','$matchid','$ort','$mail')";
-$queryupdate = "UPDATE TIPS SET HEMMAMAL_T='$hemmamal_t', BORTAMAL_T='$bortamal_t' WHERE `TIPPAR-ID`='$tipparid' AND `MATCH-ID`='$matchid'";
+$queryinsert = "INSERT INTO TIPS VALUES ('$tipparid','$matchid','$hemmamal_t','$bortamal_t','$odds')";
+$queryupdate = "UPDATE TIPS SET HEMMAMAL_T='$hemmamal_t', BORTAMAL_T='$bortamal_t', ODDS='$odds' WHERE `TIPPAR-ID`='$tipparid' AND `MATCH-ID`='$matchid'";
 
 
 
@@ -35,6 +68,7 @@ if($results['MATCH-ID']!='' && $results['TIPPAR-ID']!='')
 {
 	// utför själva frågan
 	mysqli_query($connection, $queryupdate);
+	print_r(" uppdaterar");
 	    //or die(header("location:pinnacleapitest.php"));
 		//or die();
 }
@@ -42,6 +76,7 @@ else
 {
 	// utför själva frågan
 	mysqli_query($connection, $queryinsert);
+	print_r(" insertar");
 	    //or die(header("location:pinnacleapitest.php"));
 		//or die();
 }
